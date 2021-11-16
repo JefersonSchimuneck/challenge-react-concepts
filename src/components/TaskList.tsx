548d206e7b177f2ff2a132ace914be670fdca80f
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-11-16 11:47:56 -0300

    feat: complete tests requirements

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,28 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [taskId, setTaskId] = useState(1)
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      const newTask = {id: taskId, title: newTaskTitle, isComplete: false}
+      setTasks(previousState => [...previousState, newTask])
+      setTaskId(previousState => previousState + 1)
+      setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editedTasks = tasks.map(task => task.id === id
+      ? { ...task, isComplete: !task.isComplete}
+      : task
+    )
+    setTasks(editedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
   }
 
   return (
